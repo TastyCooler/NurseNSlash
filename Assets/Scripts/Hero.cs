a98c739a48ce4d60d6d6cd68e18f010eae1f330d
TastyCooler <34125083+TastyCooler@users.noreply.github.com>
2018-01-30 19:17:44 +0100

    Movement added

--- Assets/Scripts/Hero.cs
+++ Assets/Scripts/Hero.cs
@@ -4,47 +4,36 @@ using UnityEngine;
 
 public class Hero : MonoBehaviour {
 
+    
     Rigidbody2D myRigidBody2D;
+    public float playerSpeed = 4f;
 
-	// Use this for initialization
-	void Start () {
-        myRigidBody2D = GetComponent<Rigidbody2D>();
+    // Use this for initialization
+    void Start () {
+        
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        Movement();
+        
     }
 
     private void FixedUpdate()
     {
-        
+        Movement();
     }
 
     void Movement()
     {
-        if (Input.GetKey(KeyCode.D))
-        {
-            Vector3 newPosition = transform.position;
-            transform.position += new Vector3(0.02f, 0, 0);
-        }
+        Vector2 targetVelocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        GetComponent<Rigidbody2D>().velocity = targetVelocity * playerSpeed;
+    }
 
-        if (Input.GetKey(KeyCode.W))
+    void Shoot()
+    {
+        if (Input.GetButtonDown("Fire1"))
         {
-            Vector3 newPosition = transform.position;
-            transform.position += new Vector3(0, 0.02f, 0);
-        }
 
-        if (Input.GetKey(KeyCode.A))
-        {
-            Vector3 newPosition = transform.position;
-            transform.position += new Vector3(-0.02f, 0, 0);
-        }
-
-         if (Input.GetKey(KeyCode.S))
-        {
-            Vector3 newPosition = transform.position;
-            transform.position += new Vector3(0, -0.02f, 0);
         }
     }
 }
---
+++ Assets/Scripts/Projectiles.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectiles : MonoBehaviour {
+
+    public GameObject needlePrefab;
+    public GameObject Hero;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            SpawnNeedle(Hero.transform.position);
+        }
+	}
+
+    GameObject SpawnNeedle(Vector3 position)
+    {
+        if (needlePrefab == null)
+        {
+            Debug.LogErrorFormat("No prefab reference set.");
+            return null;
+        }
+
+        //instantiate needle
+        GameObject needle = Instantiate(needlePrefab, position, Quaternion.identity);
+
+        // parent to this transform
+        needle.transform.parent = transform;
+        return needle;
+    }
+}
