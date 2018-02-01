8903372a070e4d3068edd86bcf92c4e7fa8bbacb
Eddie <Cibermon@web.de>
2018-02-01 13:31:11 +0100

    try to restore this

--- Assets/Scripts/Nurse.cs
+++ Assets/Scripts/Nurse.cs
@@ -7,19 +7,16 @@ public class Nurse : MonoBehaviour {
     int hp = 3;
     public GameObject projectile;
     Hero hero;
-    Score score;
 
 	// Use this for initialization
 	void Start () {
         hero = FindObjectOfType<Hero>();
-        score = FindObjectOfType<Score>();
+
     }
 	
 	// Update is called once per frame
 	void Update () {
-        Debug.LogFormat("Nurses hp:{0}", hp);
-        
-    }
+	}
 
     private void OnTriggerEnter2D(Collider2D trigger)
     {
@@ -30,10 +27,7 @@ public class Nurse : MonoBehaviour {
         if (hp <= 0)
         {
             
-            score.KillScore();
-           
             Destroy(gameObject);
-            
         }
         
     }
--- Assets/Scripts/TestPathFinding.cs
+++ Assets/Scripts/TestPathFinding.cs
@@ -1,33 +1,39 @@
 ﻿using UnityEngine;
-using UnityEngine.AI;
 
 public class TestPathFinding : MonoBehaviour {
 
-    /// <summary>
-    /// 
-    /// </summary>
-    public Transform victim;
-
-    /// <summary>
-    /// 
-    /// </summary>
-    private NavMeshAgent navComponent;
+    [Range(0, 0.5f)]
+    public float speed;
+<<<<<<< HEAD
+    public Hero hero;
+=======
+    public GameObject player;
+>>>>>>> af9196f8b15bdaae14b3aac8655d989f9a754c4d
 
     // Use this for initialization
-    void Start () {
-        // 
-        navComponent = GetComponent<NavMeshAgent>();
-    }
-	
-	// Update is called once per frame
-	void Update () {
-        Herofinder();
+    void Start ()
+    {
+<<<<<<< HEAD
+        hero = FindObjectOfType<Hero>();
+=======
+
+>>>>>>> af9196f8b15bdaae14b3aac8655d989f9a754c4d
     }
 
-    void Herofinder()
+    void Update()
     {
+        Find();
+    }
+
+    void Find()
+    {
+<<<<<<< HEAD
         
-            navComponent.SetDestination(victim.position);
-        
+        Vector3 localPosition = hero.transform.position - transform.position;
+=======
+        Vector3 localPosition = player.transform.position - transform.position;
+>>>>>>> af9196f8b15bdaae14b3aac8655d989f9a754c4d
+        localPosition = localPosition.normalized; // The normalized direction in LOCAL space
+        transform.Translate(localPosition.x * Time.deltaTime * speed, localPosition.y * Time.deltaTime * speed, localPosition.z * Time.deltaTime * speed);
     }
 }
