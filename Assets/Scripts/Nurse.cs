ae2b0bda608bffaed7b8dcb7cb652f2ffc066937
Eddie <Cibermon@web.de>
2018-02-01 10:17:30 +0100

    Some Game Design + Scores added

--- Assets/Scripts/Nurse.cs
+++ Assets/Scripts/Nurse.cs
@@ -7,17 +7,19 @@ public class Nurse : MonoBehaviour {
     int hp = 3;
     public GameObject projectile;
     Hero hero;
+    Score score;
 
 	// Use this for initialization
 	void Start () {
         hero = FindObjectOfType<Hero>();
-
+        score = FindObjectOfType<Score>();
     }
 	
 	// Update is called once per frame
 	void Update () {
         Debug.LogFormat("Nurses hp:{0}", hp);
-	}
+        
+    }
 
     private void OnTriggerEnter2D(Collider2D trigger)
     {
@@ -28,7 +30,10 @@ public class Nurse : MonoBehaviour {
         if (hp <= 0)
         {
             
+            score.KillScore();
+           
             Destroy(gameObject);
+            
         }
         
     }
---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Score : MonoBehaviour {
+
+    float score;
+    
+
+    // Use this for initialization
+    void Start () {
+        
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.LogFormat("score is: {0}", score);
+        
+        TimeScore();
+    }
+
+    public void KillScore()
+    {
+        score += Random.Range(10, 21);
+        Mathf.Round(score);
+    }
+
+    void TimeScore()
+    {
+        score += Time.deltaTime;
+        Mathf.Round(score);
+    }
+
+}
